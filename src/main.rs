f0519e82893cf44c247360c4022f580830b1a875
Siddhant A. Deshmukh <siddhant593@gmail.com>
2025-03-09 16:34:55 -0400

    fixed website

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
 use std::f32::consts::PI;
 
-use egui_macroquad::egui::Widget;
-use boids::*;
-use macroquad::prelude::*;
-use egui_macroquad::egui;
 use ::rand::rngs::ThreadRng;
+use boids::*;
+use egui_macroquad::egui;
+use egui_macroquad::egui::Widget;
+use macroquad::prelude::*;
 
 fn reset(rng: &mut ThreadRng) -> (Params, Vec<Boid>) {
     // Initialise parameters and boid positions
     let params = Params::default();
-    let bounds = Bounds {x_min: 0., x_max: screen_width(), y_min: 0., y_max: screen_height()};
+    let bounds = Bounds {
+        x_min: 0.,
+        x_max: screen_width(),
+        y_min: 0.,
+        y_max: screen_height(),
+    };
     // Create boids
     let num_boids = 200;
-    let boids_pop: Vec<Boid> = (0..num_boids).map(|_| random_boid(rng, &bounds)).collect::<Vec<Boid>>();
+    let boids_pop: Vec<Boid> = (0..num_boids)
+        .map(|_| random_boid(rng, &bounds))
+        .collect::<Vec<Boid>>();
     return (params, boids_pop);
 }
 
@@ -21,7 +28,7 @@ fn draw_fps(x: f32, y: f32, font_size: f32) {
     let c = match fps {
         0..=10 => RED,
         11..=30 => ORANGE,
-        _ => GREEN
+        _ => GREEN,
     };
     draw_text(format!("FPS: {}", fps).as_str(), x, y, font_size, c)
 }
@@ -32,13 +39,12 @@ macro_rules! slider {
     };
 }
 
-
 #[macroquad::main("Boids")]
 async fn main() {
     // For rendering the window
     // Set up initialisation
     let mut rng: ThreadRng = ::rand::thread_rng();
-    let (mut params, mut boids_pop) = reset(&mut rng);   
+    let (mut params, mut boids_pop) = reset(&mut rng);
     let window_color = egui::Color32::from_rgba_unmultiplied(0, 0, 0, 255);
     let mut is_paused = false;
 
@@ -59,13 +65,15 @@ async fn main() {
             let angle = velocity.y.atan2(velocity.x) * 180. / PI;
             // draw_dinosaur(boid.position.x, boid.position.y, angle, boid.color);
             draw_poly(boid.position.x, boid.position.y, 3, 6., angle, boid.color);
-            
         }
 
         // EGUI Window
         egui_macroquad::ui(|egui_ctx| {
             egui::Window::new("Controls")
-                .frame(egui::Frame{fill: window_color, ..Default::default()})
+                .frame(egui::Frame {
+                    fill: window_color,
+                    ..Default::default()
+                })
                 .fixed_pos(egui::pos2(12., 24.))
                 .show(egui_ctx, |ui| {
                     // Reset button
@@ -105,3 +113,8 @@ async fn main() {
         next_frame().await
     }
 }
+
+/* TODO:
+ * - Add time-step controls (sometimes the boids are way too fast)
+ * - Add predators that chase the prey boids
+ */
